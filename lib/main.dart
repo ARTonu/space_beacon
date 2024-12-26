19ddef65f3c12474112dcef85577105f3fc6db6f
Tonu <tonu093@gmail.com>
2024-12-26 20:51:39 +0600

    Integrate project with firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() => runApp(
-      const GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Home(),
-      ),
-    );
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
 
 class Home extends StatelessWidget {
   const Home({super.key});
